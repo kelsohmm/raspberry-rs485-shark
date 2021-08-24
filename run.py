9cc36e2c625000c873e81452657bf4cad4b94921
kelsohmm <stefanczykb@gmail.com>
2021-08-24 20:46:52 +0200

    Create run.py

---
+++ run.py
@@ -0,0 +1,25 @@
+import click
+import serial.rs485
+
+@click.command
+@click.option('--port', type=str, default='/dev/ttyAMA0')
+@click.option('--baudrate', type=int, default=9600)
+def run(port, baudrate):
+  ser = serial.rs485.RS485(
+      port=port,
+      baudrate=baudrate,
+      bytesize=serial.EIGHTBITS,
+      parity=serial.PARITY_EVEN,
+      stopbits=serial.STOPBITS_ONE,
+      timeout=1.0,
+      write_timeout=0.5,
+  )
+  ser.rs485_mode = serial.rs485.RS485Settings()
+
+  while True:
+      c = ser.read(1)
+      ser.write(c)
+      print(c, end='')
+
+if __name__ == '__main__':
+  run()
